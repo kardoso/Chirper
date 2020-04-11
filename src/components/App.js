33a65a817bf6430964b99b50dad700572a75e914
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-04-11 17:46:35 -0300

    feat: UI to create a New Tweet

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
+import NewTweet from './NewTweet'
 import LoadingBar from 'react-redux-loading'
 
 class App extends Component {
@@ -16,7 +17,7 @@ class App extends Component {
         {this.props.loading === true ?
           null
           :
-          <Dashboard />
+          <NewTweet />
         }
       </div>
     )
