85f512a38fffc136c018b62bb5f4e6af14cec6f2
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-04-11 17:30:57 -0300

    feat: Add Like Tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -6,12 +6,21 @@ import {
   TiHeartOutline,
   TiHeartFullOutline,
 } from 'react-icons/ti'
+import { handleToggleTweet } from '../actions/tweets'
 
 class Tweet extends Component {
   handleLike = (e) => {
     e.preventDefault()
 
-    //TODO: Handle Like Tweet
+    const { dispatch, tweet, authedUser } = this.props
+
+    dispatch(
+      handleToggleTweet({
+        id: tweet.id,
+        hasLiked: tweet.hasLiked,
+        authedUser,
+      })
+    )
   }
 
   toParent = (e, id) => {
